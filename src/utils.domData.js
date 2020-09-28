21f19047f02b9d12025f9a549454a2fdedbf0ccd
Lennart Pegel <github@justlep.net>
2020-09-28 17:44:24 +0200

    disposal performance++

--- src/utils.domData.js
+++ src/utils.domData.js
@@ -9,6 +9,65 @@ export const getDomData = (node, key) => {
     return dataForNode && dataForNode[key];
 };
 
+/**
+ * Returns a function that removes a given item from an array located under the node's domData[itemArrayDomDataKey].
+ * If the array IS or BECOMES empty, it will be deleted from the domData. 
+ * @return {function(Node, *): void}
+ */
+export const getCurriedDomDataArrayItemRemovalFunctionForArrayDomDataKey = (itemArrayDomDataKey) => (node, itemToRemove) => {
+    let dataForNode = node[DATASTORE_PROP],
+        itemArray = dataForNode && dataForNode[itemArrayDomDataKey];
+
+    if (itemArray) {
+        let index = itemArray.indexOf(itemToRemove);
+        if (index === 0) {
+            itemArray.shift();
+        } else if (index > 0) {
+            itemArray.splice(index, 1);
+        }
+        if (!itemArray.length) {
+            dataForNode[itemArrayDomDataKey] = undefined;
+        }
+    }
+};
+
+/**
+ * Returns a function that adds a given item to an array located under the node's domData[itemArrayDomDataKey].
+ * If the domData or the array didn't exist, either will be created.
+ * @param {string} itemArrayDomDataKey
+ * @return {function(Node, *): void}
+ */
+export const getCurriedDomDataArrayItemAddFunctionForArrayDomDataKey = (itemArrayDomDataKey) => (node, itemToAdd) => {
+    let dataForNode = node[DATASTORE_PROP] || (node[DATASTORE_PROP] = Object.create(null)),
+        itemArray = dataForNode[itemArrayDomDataKey];
+    
+    if (itemArray) {
+        itemArray.push(itemToAdd);
+    } else {
+        dataForNode[itemArrayDomDataKey] = [itemToAdd];
+    }
+}
+
+/**
+ * Returns a function that will 
+ *  (1) run all (function-)items of an array located under the node's domData[itemArrayDomDataKey], passing the node as parameter
+ *  (2) clear the node's DOM data
+ * @param {string} itemArrayDomDataKey
+ * @return {function(Node): void}
+ */
+export const getCurriedDomDataArrayInvokeEachAndClearDomDataFunctionForArrayDomDataKey = (itemArrayDomDataKey) => (node) => {
+    let dataForNode = node[DATASTORE_PROP];
+    if (dataForNode) {
+        let itemArray = dataForNode[itemArrayDomDataKey];
+        if (itemArray) {
+            for (let i = 0, _fns = itemArray.slice(0), len = _fns.length; i < len; i++) {
+                _fns[i](node);
+            }
+        }
+        delete node[DATASTORE_PROP];
+    }    
+}
+
 export const setDomData = (node, key, value) => {
     // Make sure we don't actually create a new domData key if we are actually deleting a value
     let dataForNode = node[DATASTORE_PROP] || (value !== undefined && (node[DATASTORE_PROP] = Object.create(null)));
--- src/utils.domNodeDisposal.js
+++ src/utils.domNodeDisposal.js
@@ -1,38 +1,23 @@
-import {nextDomDataKey, setDomData, getDomData, clearDomData} from './utils.domData';
+import {nextDomDataKey,
+        getCurriedDomDataArrayInvokeEachAndClearDomDataFunctionForArrayDomDataKey,
+        getCurriedDomDataArrayItemAddFunctionForArrayDomDataKey,
+        getCurriedDomDataArrayItemRemovalFunctionForArrayDomDataKey} from './utils.domData';
 import {ignoreDependencyDetection} from './subscribables/dependencyDetection';
 
-const DOM_DATA_KEY = nextDomDataKey();
+const DISPOSE_CALLBACKS_DOM_DATA_KEY = nextDomDataKey();
 const CLEANABLE_NODE_TYPES = {1: true, 8: true, 9: true};                   // Element, Comment, Document
 const CLEANABLE_NODE_TYPES_WITH_DESCENDENTS = {1: true, 8: false, 9: true}; // Element, Comment(not), Document
 
-const _getDisposeCallbacksCollection = (node, createIfNotFound) => {
-    let allDisposeCallbacks = getDomData(node, DOM_DATA_KEY);
-    if ((allDisposeCallbacks === undefined) && createIfNotFound) {
-        allDisposeCallbacks = [];
-        setDomData(node, DOM_DATA_KEY, allDisposeCallbacks);
-    }
-    return allDisposeCallbacks;
-};
-
-const _destroyCallbacksCollection = (node) => setDomData(node, DOM_DATA_KEY, undefined);
 
 /** @type {function} */
 export let _cleanExternalData = null;
 export const _overrideCleanExternalData = (fn) => _cleanExternalData = fn;
 
+const _runDisposalCallbacksAndClearDomData = getCurriedDomDataArrayInvokeEachAndClearDomDataFunctionForArrayDomDataKey(DISPOSE_CALLBACKS_DOM_DATA_KEY);
 
 const _cleanSingleNode = (node) => {
-    // Run all the dispose callbacks
-    let callbacks = _getDisposeCallbacksCollection(node, false);
-    if (callbacks) {
-        callbacks = callbacks.slice(0); // Clone, as the array may be modified during iteration (typically, callbacks will remove themselves)
-         for (let i = 0; i < callbacks.length; i++) {
-             callbacks[i](node);
-         }
-    }
-
-    // Erase the DOM data
-    clearDomData(node);
+    // Run all the dispose callbacks & ease the DOM data
+    _runDisposalCallbacksAndClearDomData(node);
 
     // Perform cleanup needed by external libraries (currently only jQuery, but can be extended)
     if (_cleanExternalData) {
@@ -42,17 +27,27 @@ const _cleanSingleNode = (node) => {
     // Clear any immediate-child comment nodes, as these wouldn't have been found by
     // node.getElementsByTagName("*") in cleanNode() (comment nodes aren't elements)
     if (CLEANABLE_NODE_TYPES_WITH_DESCENDENTS[node.nodeType]) {
-        _cleanNodesInList(node.childNodes, true/*onlyComments*/);
+        let cleanableNodesList = node.childNodes;
+        if (cleanableNodesList.length) {
+            _cleanNodesInList(cleanableNodesList, true /*onlyComments*/);
+        }
     }
 };
 
+/**
+ * @param {HTMLCollection|NodeList} nodeList
+ * @param {boolean} [onlyComments]
+ * @private
+ */
 const _cleanNodesInList = (nodeList, onlyComments) => {
-    let cleanedNodes = [], 
+    let cleanedNodes = [],
+        cleanedNodesIndex = -1,    
         lastCleanedNode;
     
-     for (let i = 0; i < nodeList.length; i++) {
-        if (!onlyComments || nodeList[i].nodeType === 8) {
-            _cleanSingleNode(cleanedNodes[cleanedNodes.length] = lastCleanedNode = nodeList[i]);
+    for (let i = 0, node; i < nodeList.length; i++) {
+        node = nodeList[i]; 
+        if (!onlyComments || node.nodeType === 8) {
+            _cleanSingleNode(cleanedNodes[++cleanedNodesIndex] = lastCleanedNode = node);
             if (nodeList[i] !== lastCleanedNode) {
                 while (i-- && !cleanedNodes.includes(nodeList[i])) {
                     // just do
@@ -62,45 +57,27 @@ const _cleanNodesInList = (nodeList, onlyComments) => {
     }
 };
 
-export const addDisposeCallback = (node, callback) => {
-    // if (typeof callback !== 'function') {
-    //     throw new Error('Callback must be a function');
-    // }
-    _getDisposeCallbacksCollection(node, true).push(callback);
-};
+/** @type {function(Node, Function): void} */
+export const addDisposeCallback = getCurriedDomDataArrayItemAddFunctionForArrayDomDataKey(DISPOSE_CALLBACKS_DOM_DATA_KEY);
 
-export const removeDisposeCallback = (node, callback) => {
-    let callbacksCollection = _getDisposeCallbacksCollection(node, false);
-    if (callbacksCollection) {
-        let index = callbacksCollection.length ? callbacksCollection.indexOf(callback) : -1;
-        if (index === 0) {
-            callbacksCollection.shift();
-        } else if (index > 0) {
-            callbacksCollection.splice(index, 1);
-        }
-        if (!callbacksCollection.length) {
-            _destroyCallbacksCollection(node);
-        }
-    }
-};
+/** @type {function(Node, Function): void} */
+export const removeDisposeCallback = getCurriedDomDataArrayItemRemovalFunctionForArrayDomDataKey(DISPOSE_CALLBACKS_DOM_DATA_KEY); 
 
 export const cleanNode = (node) => {
-    ignoreDependencyDetection(() => {
-        // First clean this node, where applicable
-        if (CLEANABLE_NODE_TYPES[node.nodeType]) {
+    if (CLEANABLE_NODE_TYPES[node.nodeType]) {
+        ignoreDependencyDetection(() => {
+            // First clean this node, where applicable
             _cleanSingleNode(node);
             // ... then its descendants, where applicable
             if (CLEANABLE_NODE_TYPES_WITH_DESCENDENTS[node.nodeType]) {
-                _cleanNodesInList(node.getElementsByTagName('*'));
+                let cleanableNodesList = node.getElementsByTagName('*');
+                if (cleanableNodesList.length) {
+                    _cleanNodesInList(cleanableNodesList);
+                }
             }
-        }
-    });
+        });
+    }
     return node;
 };
 
-export const removeNode = (node) => {
-    cleanNode(node);
-    if (node.parentNode) {
-        node.parentNode.removeChild(node);
-    }
-};
+export const removeNode = (node) => cleanNode(node).remove();
